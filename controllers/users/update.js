ea811da20952a1c3bc26b95394f6b2841e23342b
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-10-10 08:01:01 -0300

    feat: update user by ID

--- controllers/users/update.js
+++ controllers/users/update.js
@@ -3,9 +3,8 @@ import update from "../../services/user/update-user.js";
 const updateUser= async (req, res) =>{
     const id = req.query.id;
     const body = req.body;
-    await update(id,body)
-    console.log(id, body); 
-    res.send('not implemented')
+    const response = await update(id,body);
+    res.send(response);
 }
 
 export default updateUser
--- services/user/update-user.js
+++ services/user/update-user.js
@@ -1,9 +1,24 @@
-const update = async (id,body) =>{
-    if(Object.keys(body).length === 0){
-        console.log('update data inavalid')
-    }else{
-        console.log('alright');
-    }
-}
+import pool from "../../database/config.js"
 
+const update = async (id, body) => {
+    if (Object.keys(body).length === 0) {
+        return { error: 'update data invalid' };
+    }
+
+    try {
+        const result = await pool.query(
+            'UPDATE users SET name = $1, email = $2, idade = $3 WHERE id = $4',
+            [body.nome, body.email, body.idade, id]
+        );
+
+        if (result.affectedRows === 0) {
+            return { message: 'user not found' };
+        }
+
+        return { message: 'user updated' };
+    } catch (error) {
+        console.error('Error updating user:', error);
+        return { error: 'database error' };
+    }
+};
 export default update
